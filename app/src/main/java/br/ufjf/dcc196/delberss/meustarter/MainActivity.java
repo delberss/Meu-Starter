cd581e36d08c974e53c3edd3a45a51382fab9509
Delber Soares <delberss@hotmail.com>
2022-05-18 22:15:42 -0300

    testando funcao alterando ID

--- app/src/main/java/br/ufjf/dcc196/delberss/meustarter/MainActivity.java
+++ app/src/main/java/br/ufjf/dcc196/delberss/meustarter/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView textViewMensagem = findViewById(R.id.textView2);
+        TextView textViewMensagem = findViewById(R.id.textViewPadrao);
 
         textViewMensagem.setText("Olá, Mundo!");
     }
